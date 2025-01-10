315e41e3be39effd43af036a9777ac77a3195acf
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-10 16:38:24 +0100

    Inventory

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -1,5 +1,19 @@
 <x-hero>
     <section class="h-screen">
+        <div class="{{-- bg-hero h-[35vh] --}} {{-- bg-center bg-cover --}} bg-gray-100 text-black p-8 space-y-4">
+            <h1 class="text-2xl text-black">What are you looking for?</h1>
+            <input type="text" placeholder="What are you looking for"
+                class="w-full h-[38px] px-4 py-5 border rounded-lg">
+        </div>
 
+        <section>
+
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </section>
     </section>
 </x-hero>
