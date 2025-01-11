1260105820cc6250dc96dde5ad25b500ef71cc73
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-11 13:29:11 +0100

    Inv

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -6,8 +6,8 @@
             <input type="text" placeholder="What are you looking for?"
                 class="w-full h-[38px] px-4 py-5 border rounded-lg">
         </div>
-        <section class="flex gap-8">
-            <div class="w-[30%]">
+        <section class="flex gap-8 p-4">
+            <div class="w-[20%]">
                 <div class="bg-black text-white p-4 rounded-t-lg">
                     <i class="fas fa-car"></i> <span class="text-2xl font-bold">Filters</span>
                 </div>
@@ -88,16 +88,52 @@
                     </div>
                 </div>
             </div>
-            <div>
-                <div class="flex">
+            <div class="shadow-lg p-8">
+                <div class="flex space-x-8 border-b-2 py-4">
                     <div class="w-[340px]">
                         <img src="450e.jpg" alt="mercedes-benz e450" class="w-full h-[200px]">
                     </div>
-                    <div class="flex space-x-7">
+                    <div class="flex flex-col justify-between items-start">
+                        <h1 class="font-bold text-2xl">MERCEDES BENZ GLA250</h1>
+                        <h2 class="text-red text-2xl font-bold"><sup>₦</sup>14,800,000</h2>
+                        <h2 class="font-bold"><i class="fas fa-calendar-alt font-extralight text-red"></i>
+                            <span>2015</span>
+                        </h2>
+                        <section class="flex">
+                            <div class="flex items-center space-x-2">
+                                <i class="fas fa-car-battery 4x text-red p-2" style="border-right: 1px solid black"></i>
+                                <!-- Engine icon -->
+                                <div>
+                                    <h3 class="text-red text-[12px] font-bold">Engine:</h3>
+                                    <p>2.4L H4</p>
+                                </div>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <i class="fas fa-car-battery 4x text-red p-2" style="border-right: 1px solid black"></i>
+                                <!-- Engine icon -->
+                                <div>
+                                    <h3 class="text-red text-[12px] font-bold">Drivetrain:</h3>
+                                    <p>RWD</p>
+                                </div>
+                            </div>
+                            <div class="flex items-center space-x-2">
+                                <i class="fas fa-calendar-alt font-extralight text-red"></i>
+                                <!-- Engine icon -->
+                                <div>
 
+                                    <h3 class="text-red text-[12px] font-bold">Year</h3>
+                                    <p>2015</p>
+                                </div>
+                            </div>
+                        </section>
                     </div>
-
                 </div>
+                <section>
+                    <div>
+                        <h1 class="text-[14px] font-bold text-red">Capital Cars.</h1>
+                        <p>Abuja, <span class="font-bold">Nigeria</span></p>
+                    </div>
+                </section>
             </div>
         </section>
     </section>
