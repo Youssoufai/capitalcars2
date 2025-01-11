2b32000fa41caf8d9b9dd792472dca75366140f0
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-11 17:30:04 +0100

    Inv

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -88,50 +88,111 @@
                     </div>
                 </div>
             </div>
-            <div class="shadow-lg p-8">
+            <div class="shadow-lg p-8 w-[850px] h-full border">
                 <div class="flex space-x-8 border-b-2 py-4">
                     <div class="w-[340px]">
                         <img src="450e.jpg" alt="mercedes-benz e450" class="w-full h-[200px]">
                     </div>
-                    <div class="flex flex-col justify-between items-start">
+                    <div class="flex flex-col justify-between items-start px-5" style="border-right: 1px solid black">
                         <h1 class="font-bold text-2xl">MERCEDES BENZ GLA250</h1>
                         <h2 class="text-red text-2xl font-bold"><sup>₦</sup>14,800,000</h2>
                         <h2 class="font-bold"><i class="fas fa-calendar-alt font-extralight text-red"></i>
                             <span>2015</span>
                         </h2>
-                        <section class="flex">
+                        <section class="flex space">
                             <div class="flex items-center space-x-2">
-                                <i class="fas fa-car-battery 4x text-red p-2" style="border-right: 1px solid black"></i>
+                                <svg fill="#000000" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                                    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 295.238 295.238"
+                                    xml:space="preserve" width="30" height="30" stroke="red" stroke-width="1">
+                                    <g>
+                                        <g>
+                                            <g>
+                                                <path d="M257.143,157.143h-14.286v-28.571H220.49l-28.571-19.048h-30.014V80.952H200v-9.524H85.714v9.524h38.095v28.571H93.476
+                            l-33.333,28.571h-26.81v28.571H9.524V123.81H0v119.048h9.524v-38.095h23.81v38.095h27.305l33.333,19.048h53.648v-9.524H96.505
+                            l-29.838-17.048v-90.286l30.333-26h92.033l25.252,16.833v16.5h9.524v-14.286h9.524v114.286h-9.524v9.524h19.048v-23.81h14.286
+                            v9.524h38.095V142.857h-38.095V157.143z M33.334,195.238H9.524V176.19h23.81V195.238z M57.143,233.333H42.857v-85.714h14.286
+                            V233.333z M152.381,109.523h-19.048V80.952h19.048V109.523z M257.143,228.571h-14.286v-61.905h14.286V228.571z M266.667,152.381
+                            h19.048v85.714h-19.048V152.381z" stroke="red" stroke-width="red" />
+                                                <path d="M192.448,176.19h-35.576l23.81-33.333h-45.533L106.576,200h29.962l-12.833,44.933L192.448,176.19z M121.99,190.476
+                            l19.048-38.095h21.133l-23.81,33.333h31.09l-26.495,26.495l6.214-21.733H121.99z" />
+                                                <rect x="38.095" y="28.571" width="28.571" height="9.524" />
+                                                <rect y="28.571" width="28.571" height="9.524" />
+                                                <rect x="28.571" y="38.095" width="9.524" height="28.571" />
+                                                <rect x="28.571" y="0" width="9.524" height="28.571" />
+                                                <polygon
+                                                    points="276.191,71.429 276.191,52.381 266.667,52.381 266.667,71.429 276.19,71.429 276.19,80.952 266.667,80.952 
+                            266.667,71.429 247.619,71.429 247.619,80.953 266.667,80.953 266.667,100 276.191,100 276.191,80.953 295.238,80.953 
+                            295.238,71.429 " />
+                                                <rect x="190.476" y="252.381" width="23.81" height="9.524" />
+                                                <polygon
+                                                    points="180.953,252.381 190.476,252.381 190.476,228.571 180.952,228.571 180.952,252.381 157.143,252.381 
+                            157.143,261.905 180.952,261.905 180.952,285.715 190.476,285.715 190.476,261.905 180.953,261.905 " />
+                                                <polygon
+                                                    points="285.714,266.667 276.19,266.667 276.19,276.19 266.667,276.19 266.667,285.714 276.19,285.714 276.19,295.238 
+                            285.714,295.238 285.714,285.714 276.191,285.714 276.191,276.191 285.714,276.191 285.714,285.714 295.238,285.714 
+                            295.238,276.19 285.714,276.19 " />
+                                            </g>
+                                        </g>
+                                    </g>
+                                </svg>
+
                                 <!-- Engine icon -->
                                 <div>
                                     <h3 class="text-red text-[12px] font-bold">Engine:</h3>
                                     <p>2.4L H4</p>
                                 </div>
                             </div>
-                            <div class="flex items-center space-x-2">
-                                <i class="fas fa-car-battery 4x text-red p-2" style="border-right: 1px solid black"></i>
+                            <div class="flex items-center space-x-8">
+                                <i class="fas fa-car-battery 4x text-red p-2"
+                                    style="border-right: 1px solid black"></i>
                                 <!-- Engine icon -->
                                 <div>
                                     <h3 class="text-red text-[12px] font-bold">Drivetrain:</h3>
                                     <p>RWD</p>
                                 </div>
                             </div>
-                            <div class="flex items-center space-x-2">
-                                <i class="fas fa-calendar-alt font-extralight text-red"></i>
+                            <div class="flex items-center">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="12"
+                                    viewBox="0 0 32 32">
+                                    <path
+                                        d="M29.334 3h-4.334v-2c0-0.553-0.447-1-1-1s-1 0.447-1 1v2h-6v-2c0-0.553-0.448-1-1-1s-1 0.447-1 1v2h-6v-2c0-0.553-0.448-1-1-1s-1 0.447-1 1v2h-4.333c-1.473 0-2.667 1.193-2.667 2.666v23.667c0 1.473 1.194 2.667 2.667 2.667h26.667c1.473 0 2.666-1.194 2.666-2.667v-23.667c0-1.473-1.193-2.666-2.666-2.666zM30 29.333c0 0.368-0.299 0.667-0.666 0.667h-26.667c-0.368 0-0.667-0.299-0.667-0.667v-23.667c0-0.367 0.299-0.666 0.667-0.666h4.333v2c0 0.553 0.448 1 1 1s1-0.447 1-1v-2h6v2c0 0.553 0.448 1 1 1s1-0.447 1-1v-2h6v2c0 0.553 0.447 1 1 1s1-0.447 1-1v-2h4.334c0.367 0 0.666 0.299 0.666 0.666v23.667zM7 12h4v3h-4zM7 17h4v3h-4zM7 22h4v3h-4zM14 22h4v3h-4zM14 17h4v3h-4zM14 12h4v3h-4zM21 22h4v3h-4zM21 17h4v3h-4zM21 12h4v3h-4z"
+                                        fill="none" stroke="red" stroke-width="1">
+                                    </path>
+                                </svg>
+
+
                                 <!-- Engine icon -->
                                 <div>
-
                                     <h3 class="text-red text-[12px] font-bold">Year</h3>
                                     <p>2015</p>
                                 </div>
                             </div>
                         </section>
                     </div>
-                </div>
-                <section>
                     <div>
-                        <h1 class="text-[14px] font-bold text-red">Capital Cars.</h1>
-                        <p>Abuja, <span class="font-bold">Nigeria</span></p>
+                        <h2 class="font-extralight text-[14px]">Mileage</h2>
+                        <p>17,877 miles below avg.</p>
+                    </div>
+
+                </div>
+                <section class="flex justify-between items-center py-2">
+                    <div class="flex flex-col justify-between gap-2">
+                        <div>
+                            <h1 class="text-[12px] font-bold text-red">Capital Cars.</h1>
+                            <p class="text-[12px]">Abuja, <span class="font-bold text-[12px]">Nigeria</span></p>
+                        </div>
+                        <div class="flex">
+                            <svg fill="none" height="15" width="15" viewBox="0 0 16 16"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M14.5 13.5V5.41a1 1 0 0 0-.3-.7L9.8.29A1 1 0 0 0 9.08 0H1.5v13.5A2.5 2.5 0 0 0 4 16h8a2.5 2.5 0 0 0 2.5-2.5m-1.5 0v-7H8v-5H3v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1M9.5 5V2.12L12.38 5zM5.13 5h-.62v1.25h2.12V5zm-.62 3h7.12v1.25H4.5zm.62 3h-.62v1.25h7.12V11z"
+                                    clip-rule="evenodd" fill="#666" fill-rule="evenodd" />
+                            </svg>
+                            <span class="text-[12px]">Customs duty paid</span>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="px-8 py-2 bg-red text-white rounded-sm">Test Drive/Inspection</button>
                     </div>
                 </section>
             </div>
