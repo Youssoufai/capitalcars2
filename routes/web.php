f376802bdc9f028db99646d59129661db72e70d3
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-22 17:24:09 +0100

    image

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,29 @@ Route::resource('vehicles', VehicleController::class);
 
 // Update vehicle
 Route::put('/vehicle/{vehicle}', [VehicleController::class, 'update'])->name('vehicle.update');
+
+Route::get('/link', function () {
+    $target = __DIR__ . '/storage/app/public'; // The actual directory you want to link to
+    $link = __DIR__ . '/public/storage';      // The symlink location
+
+    // Check if the target directory exists
+    if (!is_dir($target)) {
+        die("The target directory does not exist: $target");
+    }
+
+    // Check if the link already exists
+    if (is_link($link)) {
+        echo "The symlink already exists: $link\n";
+    } else {
+        // Create the symlink
+        if (symlink($target, $link)) {
+            echo "Symlink created successfully: $link";
+        } else {
+            echo "Failed to create symlink.\n";
+        }
+    }
+});
+
 /* Route::get('/test-drive/book', [TestDriveController::class, 'create'])->name('test-drive.book');
 Route::post('/test-drive', [TestDriveController::class, 'store'])->name('test-drive.store'); */
 /* Route::get('/inventory', action: [VehicleController::class, 'index'])->name('inventorry');
