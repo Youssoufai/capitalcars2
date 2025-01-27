01ba554a23725ff61c97779b88148d7686b6d13b
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-27 19:14:52 +0100

    Fixing Image Path

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -120,7 +120,7 @@
                         <div class="flex flex-wrap md:flex-nowrap gap-6 border-b pb-4">
                             <div class="w-full md:w-1/3">
                                 {{-- comment --}}
-                                <img src="{{ asset('storage/' . $vehicle->image) }}" 
+                                <img src="{{ asset('/storage/car_images' . $vehicle->image) }}" 
                                 alt="{{ $vehicle->model }}" 
                                 class="w-full h-48 object-cover rounded-lg">
                            
