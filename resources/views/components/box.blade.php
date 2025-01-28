ea3fe2aa5ac78009d7d3b758c9af6b5fd7e2de81
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2025-01-28 10:09:10 +0100

    Pushing

--- resources/views/components/box.blade.php
+++ resources/views/components/box.blade.php
@@ -32,7 +32,7 @@
                 every taste and budget. Use our advanced filtering options to browse by type, model, year, and
                 more—finding your perfect ride has never been easier!</p>
             <button class='rounded px-6 md:px-8 lg:px-12 py-2 md:py-3 bg-[#EA001E] text-sm md:text-base'>View our
-                Inventory</button>
+                <a href="{{ route('inventory.index') }}">Inventory</a></button>
         </div>
     </div>
 </section>
